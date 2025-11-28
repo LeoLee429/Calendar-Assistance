accbfa1a0bf95fc1ec4c14798f54c86de85719ee
LeoLee429 <dg6546@gmail.com>
2025-11-27 21:33:40 -0500

    remove react template, implemented website

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import VoiceButton from './components/VoiceButton';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <VoiceButton />
+    </div>
+  );
+}
+
+export default App;
